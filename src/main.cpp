3512b6358a62c98526f1446f30d33335a3ece20b
bogdan.moldoveanu <bogdan.moldoveanu@SD-08>
2017-02-27 00:49:50 +0100

    Implement Otsu thresholding.

--- src/main.cpp
+++ src/main.cpp
@@ -57,11 +57,58 @@ int main( int argc, char* argv[] )
 			Scalar(255, 0, 0), 2, 8, 0);
 	}
 #pragma endregion
+	
+
+#pragma region otsu threshold computation
+	int numberOfPixels = image_grayscale.rows * image_grayscale.cols;
+	int threshold, var_max, sum, sum_B, q1, q2, miu1, miu2, sigma_t;
+	threshold = var_max = sum = sum_B = q1 = q2 = miu1 = miu2 = sigma_t = 0;
+	int max_intensity = 255;
+
+	// auxilary value for computing miu_2
+	for (int i = 0; i <= max_intensity; i++) {
+		sum += i * hist.at(i);
+	}
+
+	// update q_i(t)
+	for (int t = 0; t <= max_intensity; t++) {
+		q1 += hist.at(t);
+		if (q1 == 0) {
+			continue;
+		}
+		q2 = numberOfPixels - q1;
+
+		// update miu_i(t)
+
+		sum_B += t * hist.at(t);
+		miu1 = sum_B / q1;
+		miu2 = (sum - sum_B) / q2;
+
+		sigma_t = q1 * q2 * (miu1 - miu2) ^ 2;
+		if (sigma_t > var_max) {
+			threshold = t;
+			var_max = sigma_t;
+		}
+	}
+	//Mat output(image_grayscale.rows, image_grayscale.cols, CV_8UC(1), Scalar::all(0));;
+	//for (int i = 0; i < numberOfPixels; i++) {
+	//	//cout << (int)image_grayscale.at<uchar>(i) << " ";
+	//	if ((int)image_grayscale.at<uchar>(i) > threshold) {
+	//		output.at<uchar>(i) = 1;
+	//	}
+	//	else {
+	//		output.at<uchar>(i) = 0;
+	//	}
+	//}
+
+	Mat output = image_grayscale > (int)threshold;
+#pragma endregion
 
 
 	imwrite("hist.png", histImage);
+	imwrite("output.png", output);
 	namedWindow("image", 1);
-	imshow("image", histImage);
+	imshow("image", output);
 	waitKey();
 
 	return 0;
