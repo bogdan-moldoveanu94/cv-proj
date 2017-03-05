767d51f0c32feab6144894a65845a8bb90dd540e
bogdan.moldoveanu <bogdan.moldoveanu@SD-08>
2017-03-05 19:40:56 +0100

    Add working main.cpp before refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -33,12 +33,17 @@ int main( int argc, char* argv[] )
 	cvtColor(image_rgb, image_grayscale, CV_RGB2GRAY);
 
 #pragma region compute histogram
-	vector<int> hist(256, 0);
-	for (int row = 0; row < image_grayscale.rows; ++row) {
-		for (int column = 0; column < image_grayscale.cols; ++column) {
-			++hist[image_grayscale.at<uchar>(row, column)];
+	vector<int> hist(image_grayscale.total());
+	for (auto row = 0; row < image_grayscale.rows; row++) {
+		for (auto column = 0; column < image_grayscale.cols; column++) {
+			hist[ 0xFF & image_grayscale.at<uchar>(row, column)]++;
 		}
 	}
+
+	vector<int> histCopy = hist;
+
+
+
 	// do i really need to sort them?
 	//vector<int> indices(256);
 	//iota(indices.begin(), indices.end(), 0); // 0, 1, ... 255
@@ -60,31 +65,58 @@ int main( int argc, char* argv[] )
 	
 
 #pragma region otsu threshold computation
-	int numberOfPixels = image_grayscale.rows * image_grayscale.cols;
-	int threshold, var_max, sum, sum_B, q1, q2, miu1, miu2, sigma_t;
-	threshold = var_max = sum = sum_B = q1 = q2 = miu1 = miu2 = sigma_t = 0;
+	int numberOfPixels = image_grayscale.total();
+	float threshold, var_max, sum, sum_B, q1, q2, miu1, miu2, sigma_t;
+	threshold = var_max = sum = sum_B = q1 = q2 = miu1 = miu2 = sigma_t = 0.0;
 	int max_intensity = 255;
+	// Calculate histogram
+
+	vector<int> histData(numberOfPixels);
+	int ptr = 0;
+	while (ptr < numberOfPixels) {
+		int h = 0xFF & image_grayscale.at<uchar>(ptr);
+		histData[h] ++;
+		ptr++;
+	}
+	//int itr = 0;
+	//while(itr < numberOfPixels)
+	//{
+	//	if(histData[itr] != hist[itr])
+	//	{
+	//		cout << histData[itr] << " " << hist[itr] << endl;
+	//	}
+	//	itr++;
+	//}
+	cout << hist.size() << " and the histData " << histData.size() << endl;
 
 	// auxilary value for computing miu_2
 	for (int i = 0; i <= max_intensity; i++) {
-		sum += i * hist.at(i);
+		sum += i * histCopy.at(i);
+		// try w/ the new histogram
+		//sum += i * histData.at(i);
 	}
 
 	// update q_i(t)
 	for (int t = 0; t <= max_intensity; t++) {
-		q1 += hist.at(t);
+		q1 += histCopy.at(t);
+		// same here, try w/ new histogram
+		//q1 += histData.at(t);
 		if (q1 == 0) {
 			continue;
 		}
+
 		q2 = numberOfPixels - q1;
+		if (q2 == 0)
+			break;
 
 		// update miu_i(t)
 
-		sum_B += t * hist.at(t);
+		sum_B += t * histCopy.at(t);
+		//sum_B += t * histData.at(t);
 		miu1 = sum_B / q1;
 		miu2 = (sum - sum_B) / q2;
 
-		sigma_t = q1 * q2 * (miu1 - miu2) ^ 2;
+		sigma_t = q1 * q2 * ((miu1 - miu2) * (miu1 - miu2));
 		if (sigma_t > var_max) {
 			threshold = t;
 			var_max = sigma_t;
@@ -100,16 +132,21 @@ int main( int argc, char* argv[] )
 	//		output.at<uchar>(i) = 0;
 	//	}
 	//}
-
-	Mat output = image_grayscale > (int)threshold;
+	std::cout << "value from my impl: " << threshold << endl;
+	Mat output = image_grayscale > static_cast<int>(threshold);
+	Mat otsu;
 #pragma endregion
 
-
-	imwrite("hist.png", histImage);
-	imwrite("output.png", output);
-	namedWindow("image", 1);
-	imshow("image", output);
-	waitKey();
+	// use built-in thresholding function from opencv in order to test our implementation
+	double th_val = cv::threshold(image_grayscale, otsu, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+	std::cout << "value from built in: " << th_val << endl;
+	cv::imwrite("hist.png", histImage);
+	cv::imwrite("otsu.png", otsu);
+	cv::imwrite("output.png", output);
+	cv::namedWindow("image", 1);
+	cv::imshow("image", output);
+	cv::imshow("image2", otsu);
+	cv::waitKey();
 
 	return 0;
 	
